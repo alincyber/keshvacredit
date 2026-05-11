366060a11660b272b697111a9ea0ee7fb1d60f4d
Ajay <ajayrajputwuy@gmail.com>
2026-05-11 11:32:56 +0530

    add the local image

--- server.js
+++ server.js
@@ -4,29 +4,38 @@ require("dotenv").config();
 
 const dns = require("dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const otpRoutes = require("./routes/otpRoutes");
 const loginroutes = require("./routes/loginroutes");
 const connectDB = require("./config/db");
-
+app.use(express.static("public"));
 app.use(cors());
 app.use(express.json());
 
-// CONNECT DATABASE
+
 connectDB();
 
-// Routes
+
 app.use("/api", loginroutes);
 app.use("/api", otpRoutes);
 
+// app.get("/read", (req, res) => {
+//   res.json({
+//     message: "hi our server is online now"
+//   });
+// });
+
+
+
 app.get("/read", (req, res) => {
-  res.json({
-    message: "hi"
-  });
+    res.send(`
+        <h1>Welcome</h1>
+        <img src="/server.jpg" width="300">
+    `);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
