9c02b3136a18c44bdafc8983d08dd1b9c4108b1c
Ajay <ajayrajputwuy@gmail.com>
2026-06-04 16:32:22 +0530

    add jwt

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const cors = require("cors");
 require("dotenv").config();
 const logger = require("./config/logger");
 const requestLogger = require("./middleware/logger");
-
+const verifyToken = require('./middleware/auth');
 const dns = require("dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 
 const otpRoutes = require("./routes/otpRoutes");
 const loginroutes = require("./routes/loginroutes");
@@ -29,9 +30,13 @@ app.use(cors());
 app.use(express.json());
 
 connectDB();
+
 app.use("/api",contactroutes);
 app.use("/api", loginroutes);
 app.use("/api", otpRoutes);
+
+app.use(verifyToken);
+
 app.use("/api/loan", loanroutes);
 app.use("/api/company", companyroutes);
 app.use("/api/business", businessroutes);
