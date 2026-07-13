6a5566f7164204f235894d6ec783247af898ac4b
Ajay <ajayrajputwuy@gmail.com>
2026-07-13 12:51:22 +0530

    the create user  database change in the perosonal

--- controllers/deleteusercontroller.js
+++ controllers/deleteusercontroller.js
@@ -1,4 +1,4 @@
-const User = require("../model/userdata");
+const User = require("../model/partnermodel");
 const DeletedUser = require("../model/deleteusermodel");
 const logger = require("../config/logger");
 
